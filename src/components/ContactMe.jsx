9d78d2325ca26df8dd21cf79e8a9b3f234875f9c
unknown <Shobhan>
2025-02-12 21:50:30 +0530

    added navbar and other 2 pages

---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactMe = () => {
+    console.log("ContactMe component rendered!");
+    return (
+        <div className="pt-4"> {/* Adds space for the navbar */}
+            <h1>This is the Contact Me page</h1>
+        </div>
+    )
+}
+
+export default ContactMe
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div className='pt-16'>
+            <h1 className='text-2xl'>I am a project developer</h1>
+        </div>
+    )
+}
+
+export default Projects
